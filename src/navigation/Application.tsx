407e347fcff63fd8810b96bc1871a165325e3ffc
jody816 <81346079+jody816@users.noreply.github.com>
2025-01-28 18:57:02 +0900

    feat: 타입 설정 및 스크린 추가

--- src/navigation/Application.tsx
+++ src/navigation/Application.tsx
@@ -1,19 +1,23 @@
-import { Paths } from "@/src/navigation/paths";
-import { NavigationContainer } from "@react-navigation/native";
-import { SafeAreaProvider } from "react-native-safe-area-context";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import {Home} from "@/src/screens";
+import {Paths} from '@/src/navigation/paths';
+import {NavigationContainer} from '@react-navigation/native';
+import {SafeAreaProvider} from 'react-native-safe-area-context';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {Home, Example} from '@/src/screens';
+import {RootStackParamList} from './types';
 
-const Stack = createNativeStackNavigator();
+const Stack = createNativeStackNavigator<RootStackParamList>();
 
 export default function ApplicationNavigator() {
   return (
     <SafeAreaProvider>
-        <NavigationContainer>
-            <Stack.Navigator initialRouteName="home" screenOptions={{ headerShown: false }}>
-                <Stack.Screen name={Paths.Home} component={Home} />
-            </Stack.Navigator>
-        </NavigationContainer>
+      <NavigationContainer>
+        <Stack.Navigator
+          initialRouteName={Paths.Home}
+          screenOptions={{headerShown: false}}>
+          <Stack.Screen name={Paths.Home} component={Home} />
+          <Stack.Screen name={Paths.Example} component={Example} />
+        </Stack.Navigator>
+      </NavigationContainer>
     </SafeAreaProvider>
-  )
+  );
 }
