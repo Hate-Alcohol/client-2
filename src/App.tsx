4d99eaeedc2c2fda9fe7c90b84a3bcfc6bd2b480
jody816 <81346079+jody816@users.noreply.github.com>
2025-01-28 18:37:27 +0900

    feat: 리액트 쿼리 설정 추가
    여기서 리액트 쿼리에 대한 전역 설정을 할 수 있음

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,19 @@
 import * as React from 'react';
-import { MMKV } from "react-native-mmkv";
-import { GestureHandlerRootView } from "react-native-gesture-handler";
-import ApplicationNavigator from "@/src/navigation/Application";
+import {MMKV} from 'react-native-mmkv';
+import {GestureHandlerRootView} from 'react-native-gesture-handler';
+import ApplicationNavigator from '@/src/navigation/Application';
+import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
 
 export const storage = new MMKV();
 
+const queryClient = new QueryClient();
+
 export default function App() {
   return (
-    <GestureHandlerRootView>
+    <QueryClientProvider client={queryClient}>
+      <GestureHandlerRootView>
         <ApplicationNavigator />
-    </GestureHandlerRootView>
+      </GestureHandlerRootView>
+    </QueryClientProvider>
   );
 }
